9dd8a8e3cb04ccde04daf775e58748fa477f3f02
Abdul Rauf <abdulraufmujahid@gmail.com>
2018-06-10 17:24:17 +0500

    fix: depreciated navigator warnings

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
 import History from './components/History'
-import { TabNavigator, StackNavigator } from 'react-navigation'
+import { createBottomTabNavigator, createStackNavigator } from 'react-navigation'
 import { purple, white } from './utils/colors'
 import { FontAwesome, Ionicons } from '@expo/vector-icons'
 import { Constants } from 'expo'
@@ -21,7 +21,7 @@ function UdaciStatusBar ({backgroundColor, ...props}) {
   )
 }
 
-const Tabs = TabNavigator({
+const Tabs = createBottomTabNavigator({
   History: {
     screen: History,
     navigationOptions: {
@@ -63,7 +63,7 @@ const Tabs = TabNavigator({
   }
 })
 
-const MainNavigator = StackNavigator({
+const MainNavigator = createStackNavigator({
   Home: {
     screen: Tabs,
   },
